8e431d66ba570e8026c7aa05238d41becab8a23e
Guilherme R <grsguiga@gmail.com>
2022-01-27 16:25:24 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const allAnimals = {};
+    species.forEach((specie) => {
+      allAnimals[specie.name] = specie.residents.length;
+    });
+    return allAnimals;
+  }
+  if (!animal.sex) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  return species.find((specie) => specie.name === animal.specie)
+    .residents.filter((sexo) => sexo.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
