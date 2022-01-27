493ae7e4e056fdaba5282eaa03c5600c4376674f
Guilherme R <grsguiga@gmail.com>
2022-01-27 17:48:28 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const criança = entrants.filter((person) => person.age < 18).length;
+  const adulto = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const idoso = entrants.filter((person) => person.age >= 50).length;
+  const entradas = {
+    adult: adulto,
+    child: criança,
+    senior: idoso,
+  };
+  return entradas;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const calc = countEntrants(entrants);
+
+  return calc.child * prices.child + calc.adult * prices.adult + calc.senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
